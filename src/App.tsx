322f0e5ddf2fff68b729315818254b4ce78c53cd
Robin Kahlow <robinkahlow@web.de>
2020-07-11 20:52:35 +0100

    add routing and breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,29 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useMemo } from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from "react-router-dom"
 import './App.css'
 import { InteractiveCode } from "./InteractiveCode"
 import * as ga from "./ga/ga_pp"
 import * as pga from "./ga/ga_zpp"
 import * as viz from "./ga/viz2d"
+import * as cnt from "./content"
 
-const visualizerExample = `// Render a point at x: 50, y: 10
-renderPointPGA({
-    e02: -50,
-    e01: 10,
-    e12: 1
-}, "lime");
+const pathToBreadcrumbs = (path: string) => {
+    const pathNames = ["Geometric Algebra"]
+    const pathUrls = ["/"]
 
-// Render the line 1x + 1y = 10
-renderLinePGA({
-    e0: 10,
-    e1: 1,
-    e2: -1
-}, "orange");`
+    let pathSplit = path.substr(1).split("/")
 
-const textA = `
-Usually introductions to GA begin by defining various rules and going over derivations before doing anything useful with them. I will also define some rules but try to get to the interesting stuff more quickly.
-`
+    while (pathSplit.length > 0) {
+        pathNames.push(pathSplit[0])
+        pathUrls.push(`${pathUrls[pathUrls.length - 1]}/${pathSplit[0]}`)
+        pathSplit.splice(0, 1)
+    }
 
-const textA2 = `
-Like for the standard 2D vector algebra in geometric algebra we have two basis vectors $e_x, e_y$ using which arbitrary vectors $v = x e_x + y e_y$ can be formed. Below is some runnable and editable code that forms
-such a vector and then displays them. The basis vectors $e_x, e_y$ are labeled e0 and e1 in the code.
-`
-
-const codeA2 = `// Render point at x=10, y=-60
-renderPointGA({ e0: 10, e1: -60 })
-
-// Render point at x=-50, y=80
-renderPointGA({ e0: -50, e1: 80}, "red")
-`
-
-const textA3 = `Multiplying two same basis vectors together will result in $+1$ if they are the same (ie. $e_x e_x = 1, e_y e_y = 1$) similarly to how the dot product in standard vector algebra works.
-The product which defines Geometric Algebra and is its most important aspect is called the Geometric Product. Let's verify these results with the code again this time just logging some text instead of visualizing.
-`
-
-const codeA3 = `console.log("e0^2:", ga.geometricProduct({ e0: 1 }, { e0: 1 }))
-console.log("e1^2:", ga.geometricProduct({ e1: 1 }, { e1: 1 }))`
-
-const textB = `What is new is that we can also multiply two different basis vectors and the result will not be zero, but can't be simplified further $e_x e_y = e_{xy}$.
-$e_xy$ here is not something new, it just stands for the two basis vectors multiplied together as a shorthand way of writing. These elements made up of two basis vectors are called bivectors.
-`
-
-const textB2 = `
-Importantly the order of the product matters. A rule is that when you swap the factors of a product of basis vectors you pick up a minus sign, for example: $e_{xy} = e_x e_y = -e_y e_x = -e_{yx}$.
-`
-
-const codeB = `console.log("e0 e1:", ga.geometricProduct({ e0: 1 }, { e1: 1 }))
-console.log("e1 e0:", ga.geometricProduct({ e1: 1 }, { e0: 1 }))`
-
-const textC = `
-Let's now use these three basic rules we just learnt and see what some results are when we use them:
-
-\\begin{aligned}
-e_x e_y e_x & = & \\text{(rewrite as shorthand)} \\\\
-e_{xyx} & = & \\text{(Swap neighbours on the right, pick up minus sign)} \\\\
--e_{xxy} & = & \\text{(Multiplying same basis vectors results in 1, e_xx = e_x e_x = 1)} \\\\
--e_y &
-\\end{aligned}
-
-We can verify these results with the code:
-`
-
-const codeC = `var a = ga.geometricProduct({ e0: 1 }, { e1: 1 }) // e_x e_y
-var b = ga.geometricProduct(a, { e0: 1 }) // e_x e_y e_x
-console.log("e0 e1 e0:", b)`
-
-const textD = `Hopefully this gives some idea of how the rules work and how to use them. Now for something more interesting, let's see what happens if we multiply of these together, that is, squaring it:
-`
-
-const codeD = `console.log("e01^2", ga.geometricProduct({ e01: 1 }, { e01: 1 }))`
-
-const textE = `
-\\begin{equation}
-e_{xy}^2 = e_{xy} e_{xy} = e_{xyxy} = -e_{xyyx} = -e_{xx} = -1
-\\end{equation}
-
-We can see the square of the bivector $e_{xy}$ is $-1$, the same as the square of the imaginary unit of the complex numbers.
-`
-
-const textF = `
-As mentioned before $e_{xy}$ can be identified as the imaginary unit $i$ of complex numbers hence we can represent complex numbers as $a + b e_{xy}$ and a rotation by an arbitrary angle $\\phi$
-can be performed just like with complex numbers using Euler's formula
-
-\\begin{equation}
-R(\\phi) = e^{\\phi e_{xy}} = cos(\\phi) + e_{xy} sin(\\phi)
-\\end{equation}
-
-The object $R$ you get after exponentiating is called a rotor (because it rotates when you multiply with it, d'uh).
-
-Unlike with complex numbers now however, we can multiply a vector by a rotor directly instead of having to treat vectors as if they were complex numbers.
-`
-
-const codeF = `var phi = Math.PI * 3 / 4 // 3/4 pi is 135°
-
-// e^(phi e_xy)
-var r = ga.exponential(
-    ga.geometricProduct(
-        { scalar: phi },
-        { e01: 1 }
-    )
-)
-
-var p = { e0: 70, e1: 0 }
-
-var rotatedP = ga.geometricProduct(r, p) // p rotated by 45°
-
-renderPointGA({ e0: 0, e1: 0 }, "purple") // Origin
-renderPointGA(p)
-renderPointGA(rotatedP, "red")`
-
-const textG = `
-\\begin{equation}
-v R(\\phi) = (x e_x + y e_y) (cos(\\phi) + e_{xy} sin(\\phi)) = e_x (x cos(\\phi) - y sin(\\phi)) + e_y (x sin(\\phi) + y cos(\\phi))
-\\end{equation}
-
-We can see that the imaginary unit is actually a rotation in the XY plane as the bivector $e_{xy}$ is formed by multiplying the two basis vectors together, so in some sense it represents the XY plane.
-`
+    return pathNames.map((name, i) => {
+        return { name: name, url: pathUrls[i] }
+    })
+}
 
 function GATutorial() {
     // Need to retrigger equation typesetting as it's only done once on startup
@@ -134,35 +35,35 @@ function GATutorial() {
     return (
         <div>
             <h3>Geometric Algebra Basics</h3>
-            <div>{textA}</div>
-            <div>{textA2}</div>
+            <div>{cnt.textA}</div>
+            <div>{cnt.textA2}</div>
 
-            <InteractiveCode sourceCode={codeA2}
+            <InteractiveCode sourceCode={cnt.codeA2}
                 hideOutput={true} withVisualizer={true}
             />
 
-            <div>{textA3}</div>
-            <InteractiveCode sourceCode={codeA3} />
+            <div>{cnt.textA3}</div>
+            <InteractiveCode sourceCode={cnt.codeA3} />
 
-            <div>{textB}</div>
-            <div>{textB2}</div>
-            <InteractiveCode sourceCode={codeB} />
+            <div>{cnt.textB}</div>
+            <div>{cnt.textB2}</div>
+            <InteractiveCode sourceCode={cnt.codeB} />
 
-            <div>{textC}</div>
-            <InteractiveCode sourceCode={codeC} />
+            <div>{cnt.textC}</div>
+            <InteractiveCode sourceCode={cnt.codeC} />
 
-            <div>{textD}</div>
-            <InteractiveCode sourceCode={codeD} />
+            <div>{cnt.textD}</div>
+            <InteractiveCode sourceCode={cnt.codeD} />
 
-            <div>{textE}</div>
+            <div>{cnt.textE}</div>
 
             <h3>Using GA to perform 2D rotations</h3>
-            <div>{textF}</div>
-            <InteractiveCode sourceCode={codeF}
+            <div>{cnt.textF}</div>
+            <InteractiveCode sourceCode={cnt.codeF}
                 hideOutput={true} withVisualizer={true}
             />
 
-            <div>{textG}</div>
+            <div>{cnt.textG}</div>
 
             <h4><Link to="/pga">2. Projective Geometric Algebra</Link></h4>
         </div>
@@ -179,7 +80,7 @@ function PGATutorial() {
     return (
         <div>
             <h3>Points and Lines</h3>
-            <InteractiveCode sourceCode={visualizerExample}
+            <InteractiveCode sourceCode={cnt.visualizerExample}
                 hideOutput={true} withVisualizer={true} />
         </div>
     )
@@ -201,6 +102,49 @@ function TutorialIndex() {
                 <li><Link to="/pga">Projective Geometric Algebra</Link></li>
             </ol>
         </div>
+    )
+}
+
+function TutorialSite() {
+    const location = useLocation()
+
+    const breadcrumbs = useMemo(() => {
+        const crumbs = pathToBreadcrumbs(location.pathname)
+
+        return crumbs
+            .map(c => <a href={c.url}>{c.name}</a>)
+            .reduce((l, r) => <> {l} &gt; {r}  </>)
+    }, [location])
+
+    return (
+        <>
+            <div className="demo-container mdl-grid">
+                <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+                <div
+                    className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+                    <div className="demo-crumbs mdl-color-text--grey-500">
+                        {breadcrumbs}
+                    </div>
+                    <Switch>
+                        <Route path="/ga-basics">
+                            <GATutorial />
+                        </Route>
+                        <Route path="/pga">
+                            <PGATutorial />
+                        </Route>
+                        <Route path="/">
+                            <TutorialIndex />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+            <footer className="demo-footer mdl-mini-footer">
+                <div className="mdl-mini-footer--left-section">
+                    <ul className="mdl-mini-footer--link-list">
+                    </ul>
+                </div>
+            </footer>
+        </>
     )
 }
 
@@ -226,17 +170,7 @@ function App() {
 
     return (
         <Router>
-            <Switch>
-                <Route path="/ga-basics">
-                    <GATutorial />
-                </Route>
-                <Route path="/pga">
-                    <PGATutorial />
-                </Route>
-                <Route path="/">
-                    <TutorialIndex />
-                </Route>
-            </Switch>
+            <TutorialSite />
         </Router>
     )
 }
---
+++ src/content.ts
@@ -0,0 +1,116 @@
+export const visualizerExample = `// Render a point at x: 50, y: 10
+renderPointPGA({
+    e02: -50,
+    e01: 10,
+    e12: 1
+}, "lime");
+
+// Render the line 1x + 1y = 10
+renderLinePGA({
+    e0: 10,
+    e1: 1,
+    e2: -1
+}, "orange");`
+
+export const textA = `
+Usually introductions to GA begin by defining various rules and going over derivations before doing anything useful with them. I will also define some rules but try to get to the interesting stuff more quickly.
+`
+
+export const textA2 = `
+Like for the standard 2D vector algebra in geometric algebra we have two basis vectors $e_x, e_y$ using which arbitrary vectors $v = x e_x + y e_y$ can be formed. Below is some runnable and editable code that forms
+such a vector and then displays them. The basis vectors $e_x, e_y$ are labeled e0 and e1 in the code.
+`
+
+export const codeA2 = `// Render point at x=10, y=-60
+renderPointGA({ e0: 10, e1: -60 })
+
+// Render point at x=-50, y=80
+renderPointGA({ e0: -50, e1: 80}, "red")
+`
+
+export const textA3 = `Multiplying two same basis vectors together will result in $+1$ if they are the same (ie. $e_x e_x = 1, e_y e_y = 1$) similarly to how the dot product in standard vector algebra works.
+The product which defines Geometric Algebra and is its most important aspect is called the Geometric Product. Let's verify these results with the code again this time just logging some text instead of visualizing.
+`
+
+export const codeA3 = `console.log("e0^2:", ga.geometricProduct({ e0: 1 }, { e0: 1 }))
+console.log("e1^2:", ga.geometricProduct({ e1: 1 }, { e1: 1 }))`
+
+export const textB = `What is new is that we can also multiply two different basis vectors and the result will not be zero, but can't be simplified further $e_x e_y = e_{xy}$.
+$e_xy$ here is not something new, it just stands for the two basis vectors multiplied together as a shorthand way of writing. These elements made up of two basis vectors are called bivectors.
+`
+
+export const textB2 = `
+Importantly the order of the product matters. A rule is that when you swap the factors of a product of basis vectors you pick up a minus sign, for example: $e_{xy} = e_x e_y = -e_y e_x = -e_{yx}$.
+`
+
+export const codeB = `console.log("e0 e1:", ga.geometricProduct({ e0: 1 }, { e1: 1 }))
+console.log("e1 e0:", ga.geometricProduct({ e1: 1 }, { e0: 1 }))`
+
+export const textC = `
+Let's now use these three basic rules we just learnt and see what some results are when we use them:
+
+\\begin{aligned}
+e_x e_y e_x & = & \\text{(rewrite as shorthand)} \\\\
+e_{xyx} & = & \\text{(Swap neighbours on the right, pick up minus sign)} \\\\
+-e_{xxy} & = & \\text{(Multiplying same basis vectors results in 1, e_xx = e_x e_x = 1)} \\\\
+-e_y &
+\\end{aligned}
+
+We can verify these results with the code:
+`
+
+export const codeC = `var a = ga.geometricProduct({ e0: 1 }, { e1: 1 }) // e_x e_y
+var b = ga.geometricProduct(a, { e0: 1 }) // e_x e_y e_x
+console.log("e0 e1 e0:", b)`
+
+export const textD = `Hopefully this gives some idea of how the rules work and how to use them. Now for something more interesting, let's see what happens if we multiply of these together, that is, squaring it:
+`
+
+export const codeD = `console.log("e01^2", ga.geometricProduct({ e01: 1 }, { e01: 1 }))`
+
+export const textE = `
+\\begin{equation}
+e_{xy}^2 = e_{xy} e_{xy} = e_{xyxy} = -e_{xyyx} = -e_{xx} = -1
+\\end{equation}
+
+We can see the square of the bivector $e_{xy}$ is $-1$, the same as the square of the imaginary unit of the complex numbers.
+`
+
+export const textF = `
+As mentioned before $e_{xy}$ can be identified as the imaginary unit $i$ of complex numbers hence we can represent complex numbers as $a + b e_{xy}$ and a rotation by an arbitrary angle $\\phi$
+can be performed just like with complex numbers using Euler's formula
+
+\\begin{equation}
+R(\\phi) = e^{\\phi e_{xy}} = cos(\\phi) + e_{xy} sin(\\phi)
+\\end{equation}
+
+The object $R$ you get after exponentiating is called a rotor (because it rotates when you multiply with it, d'uh).
+
+Unlike with complex numbers now however, we can multiply a vector by a rotor directly instead of having to treat vectors as if they were complex numbers.
+`
+
+export const codeF = `var phi = Math.PI * 3 / 4 // 3/4 pi is 135°
+
+// e^(phi e_xy)
+var r = ga.exponential(
+    ga.geometricProduct(
+        { scalar: phi },
+        { e01: 1 }
+    )
+)
+
+var p = { e0: 70, e1: 0 }
+
+var rotatedP = ga.geometricProduct(r, p) // p rotated by 45°
+
+renderPointGA({ e0: 0, e1: 0 }, "purple") // Origin
+renderPointGA(p)
+renderPointGA(rotatedP, "red")`
+
+export const textG = `
+\\begin{equation}
+v R(\\phi) = (x e_x + y e_y) (cos(\\phi) + e_{xy} sin(\\phi)) = e_x (x cos(\\phi) - y sin(\\phi)) + e_y (x sin(\\phi) + y cos(\\phi))
+\\end{equation}
+
+We can see that the imaginary unit is actually a rotation in the XY plane as the bivector $e_{xy}$ is formed by multiplying the two basis vectors together, so in some sense it represents the XY plane.
+`
