e2f834b8922b56d1af362b92f996fe144b38dd8b
Robin Kahlow <robinkahlow@web.de>
2020-07-11 23:43:43 +0100

    use react-snap for static build

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import { hydrate, render } from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function getApp() {
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    )
+}
+
+const rootElement = document.getElementById("root")
+if (rootElement) {
+    if (rootElement.hasChildNodes()) {
+        hydrate(getApp(), rootElement)
+    } else {
+        render(getApp(), rootElement)
+    }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://bit.ly/CRA-PWA 
+serviceWorker.unregister()
