447b1dffa1da3fd3f365cad7b64c900f5c9bfb77
RobinKa <xtremegosugaming@googlemail.com>
2020-07-12 02:05:23 +0000

    Deploying to gh-pages from  @ a6b8d6a143c0e4263014d270082d5c5abd7f383c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a1ddf0a62154b7d5eaa961461391780.js"
+  "/precache-manifest.8a500d3a29c94ee2002426d70fb4954c.js"
 );
 
 self.addEventListener('message', (event) => {
