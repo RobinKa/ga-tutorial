8786c738873bc99749e53d27193305d6bf0a1df8
Robin Kahlow <robinkahlow@web.de>
2021-01-18 00:55:43 +0000

    meet and join

--- src/pages/GADesign1.tsx
+++ src/pages/GADesign1.tsx
@@ -69,7 +69,7 @@ export function GADesign1() {
                 
                 In the first part of this article we will try to figure out how to create a
                 GA that contains the objects we want. Specifically we will try to create a
-                GA containing <a href="https://en.wikipedia.org/wiki/Elliptic_curve">elliptic curves</a>.
+                GA containing arbitrary polynomial shapes such as parabolas and <a href="https://en.wikipedia.org/wiki/Elliptic_curve">elliptic curves</a>.
                 In the second part we will also look at the rotors.
             </div>
 
@@ -91,14 +91,15 @@ export function GADesign1() {
             <h4>Object representation in GA</h4>
             <div>
                 <div>
-                    Now jumping to GA, in 2D PGA a point is represented by a bivector {`$x e_{0y} + y e_{x0} + 1 e_{xy}$`} 
-                    and a line is represented by a vector {`$x e_x + y e_y - d e_0$`}. Let's call the mapping for points
+                    Now jumping to GA, in 2D PGA a point is represented by a bivector {`$x e_{0y} + y e_{x0} + 1 e_{xy}$`} and 
+                    a line is represented by a vector {`$x e_x + y e_y - d e_0$`}. Let's call the mapping for points
                     the $up$  function:
                     {`\\begin{equation}
                     up: \\mathbb{R}^N \\mapsto \\mathbb{Cl}(...)
                     \\end{equation}`}
 
-                    Here's a table of the previously mentioned algebras and their up functions.
+                    Here's a table of the previously mentioned algebras and their up functions. Note that some of these use a dual representation 
+                    for points (points as pseudovectors) and some use the usual representation (points as vectors).
 
                     <table style={{ width: "100%", padding: "2%" }}>
                         <tr>
@@ -133,8 +134,7 @@ export function GADesign1() {
                 <h5>Inner Product Null Space Representation</h5>
                 <div>
                     The first one is the Inner Product Null Space (IPNS) representation. Here we say that the inner product
-                    of our object, let's call it $o$, is zero with arbitrary other objects of the same grade if the other 
-                    object lies on our object. For example if our object $o$ is a vector and $X$ is an arbitrary vector then we get
+                    of our object, let's call it $o$, is zero for arbitrary points produced by our up function.
 
                     {`\\begin{equation}
                     X \\cdot o = 0
@@ -148,29 +148,34 @@ export function GADesign1() {
                 <div>
                     The second representation is the Outer Product Null Space (OPNS) representation. Instead of the inner product 
                     we use the outer product which means we don't need to define a metric either and this will work the same regardless 
-                    of what our basis vectors square to. We wedge with an arbitrary object with the dual grade of the object 
-                    we're wedging with. For example if the object $o$ is a vector then the arbitrary $X$ will be a pseudovector.
+                    of what our basis vectors square to. We wedge our object $o$ with an arbitrary point produced by our up function.
 
                     {`\\begin{equation}
                     X \\wedge o = 0
                     \\end{equation}`}
 
+                    The intuition here is that if points are represented by vectors then $\wedge$ represents the join operation. If 
+                    we try to join a point that already lies on the object we get zero. Note that if we're using a dual representation 
+                    (points as pseudovectors) then we need to use $\vee$ instead as that is then the join operation (maybe a better name 
+                    would then be the Vee Product Null Space (VPNS)?). 
+                </div>
+                <div>
                     We will continue using OPNS from now on and apply it to recover more familiar equations for the objects.
                 </div>
                 <h5>OPNS applied to PGA 2D</h5>
                 <div>
                     Let's apply the OPNS equation to see what a vector in 2D PGA represents. For the arbitrary $X$ 
                     we need to use a pseudovector. The $up(x, y)$ function gives us a pseudovector (bivector in this case) 
-                    so let's use it in place of $X$
+                    so let's use it in place of $X$ and remember that we need to use $\vee$ for dual representations.
 
                     {`\\begin{aligned}
-                    up(x, y) \\wedge vector & = (x e_{0y} + y e_{x0} + 1 e_{xy}) \\wedge (a e_x + b e_y - d e_0)  & = \\\\
-                    & = a x e_{0xy} + b y e_{0xy} - d e_{0xy}  & =  \\\\
-                    & = a x + b y - d = 0 &
+                    up(x, y) \\vee vector & = (x e_{0y} + y e_{x0} + 1 e_{xy}) \\vee (a e_x + b e_y + d e_0) \\\\
+                    & = a x + b y + d  \\\\
+                    & = a x + b y + d = 0
                     \\end{aligned}`}
 
                     We have recovered the usual equation for a line. Unlike the previous one, this one also has a coefficient for $y$ which makes 
-                    it possible to represent vertical lines (by setting $b = 0$ we get $a x = d$). Hence the vectors in 2D PGA represent lines.
+                    it possible to represent vertical lines (by setting $b = 0$ we get $a x = -d$). Hence the vectors in 2D PGA represent lines.
                 </div>
             </div>
 
@@ -184,7 +189,7 @@ export function GADesign1() {
             <div>
                 As an example, if we wanted to represent parabolas, we would need to have a basis vector that has $x^2$ as a 
                 coefficient in $up$. For example {`$up(x, y) = x^2 e_{0y} + y e_{x0} + 1 e_{xy}$`} 
-                would give us $a x^2 + b y - d = 0$ after applying the OPNS equation.
+                would give us $a x^2 + b y + d = 0$ after applying the OPNS equation.
             </div>
             <div>
                 Another interesting observation here is that we have a constant term in the polynomial 
@@ -206,19 +211,20 @@ export function GADesign1() {
                 4 terms in our up function with coefficients $y^2, x^3, x, 1$. As a result we will need 4 basis vectors (with any signature). 
 
                 {`\\begin{aligned}
-                up(x, y) & = & x^3 e_1^* + y^2 e_2^* + x e_3^* + 1 e_4^* = \\\\
-                & = & x^3 e_{234} + y^2 e_{134} + x e_{124} + 1 e_{123}
+                up(x, y) & = x^3 e_1^* + y^2 e_2^* + x e_3^* + 1 e_4^* \\\\
+                & = x^3 e_{234} + y^2 e_{134} + x e_{124} + 1 e_{123}
                 \\end{aligned}`}
 
                 Vectors will now be able to represent elliptic curves as well as other simpler curves (parabolas, cubics, lines, ...).
-                You can verify this by hand by applying the OPNS equation to a vector as before in the 2D PGA example.
+                You can verify this by hand (or ideally using symbolic math software) by applying the OPNS equation to a vector as 
+                before in the 2D PGA example.
             </div>
 
-            <h4>Join and meet (WIP)</h4>
+            <h4>Join and meet</h4>
             <div>
                 <div>
                     If points are represented by vectors the join is $\wedge$ and meet is $\vee$.
-                    In algebras that uses a dual representation for points such as PGA this is reversed and join is $\vee$ 
+                    In algebras that uses a dual representation for points such as PGA this is swapped and join is $\vee$ 
                     and meet is $\wedge$.
                 </div>
                 <div>
@@ -229,11 +235,87 @@ export function GADesign1() {
                     However the representation for such a point pair is identical to the representation of a single point in PGA.
                     The difference will only be visible once we apply the OPNS equation to find out what our object is actually representing.
                 </div>
+                <h5>Example: parabola at origin intersects horizontal line</h5>
+                <div>
+                    Let's apply what we have just learnt and look at some actual results to see if they make sense.
+                    We want to intersect a parabola at the origin of the form $y = a x^2$ with a horizontal line of the form 
+                    $y = c 1$. We would expect the intersection to contain 2 points (assuming they intersect).
+                </div>
+                <div>
+                    From the two equations we see that we need the terms $x^2$, $y$ and $1$ and thus we require 3 basis vectors. We'll use a dual 
+                    representation (points as pseudovectors) and define our up function as follows
+
+                    {`\\begin{aligned}
+                    up(x, y) & = x^2 e_1^* + y e_2^* + 1 e_3^* \\\\
+                    & = x^2 e_{23} + y e_{31} + 1 e_{12}
+                    \\end{aligned}`}
+
+                    Now curves should be represented by vectors. For our parabola $p$ we need the $x^2$ and $y$ basis vectors, that is $e_1$ and $e_2$ respectively.
+                    For our line $l$ we will need the $y$ and $1$ basis vectors, that is $e_2$ and $e_3$ respectively.
+
+                    {`\\begin{aligned}
+                    p & = a e_1 - e_2 \\\\
+                    l & = -e_2 + b e_3
+                    \\end{aligned}`}
+
+                    Let's look at the OPNS (using $\vee$) for $p$ to confirm it indeed represents the desired parabola.
+
+                    {`\\begin{aligned}
+                    up(x, y) \\vee p & = (x^2 e_1^* + y e_2^* + 1 e_3^*) \\vee (a e_1 - e_2) \\\\
+                    & = a x^2 - y = 0 \\\\
+                    \\end{aligned}`}
+
+                    {`\\begin{aligned}
+                    up(x, y) \\vee l & = (x^2 e_1^* + y e_2^* + 1 e_3^*) \\vee (-e_2 + b e_3) \\\\
+                    & = b - y = 0 \\\\
+                    \\end{aligned}`}
+
+                    Looks like we're good! Now we can intersect the two objects using $\wedge$ as that is the meet operation in the dual representation.
+                    We will call the intersection object $i$.
+                    
+                    {`\\begin{aligned}
+                    i = p \\wedge l = -a e_{12} + a b e_{13} - b e_{23}
+                    \\end{aligned}`}
+
+                    Now let's look at the OPNS of the intersection object to see if it gives us what we expect (two points of intersection).
+
+                    {`\\begin{aligned}
+                    up(x, y) \\vee i & = (x^2 e_1^* + y e_2^* + 1 e_3^*) \\vee (-a e_{12} + a b e_{13} - b e_{23}) \\\\
+                    & = a (-b + y) e_1 + (-a x^2 + b) e_2 + b (a x^2 - y) e_3 = 0
+                    \\end{aligned}`}
+
+                    Now we have three vectors that need to each be equal to zero.
+
+                    {`\\begin{aligned}
+                    a (-b + y) = 0 & \\implies y = b \\\\
+                    (-a x^2 + b) = 0 & \\implies x^2 = b / a \\\\
+                    b (a x^2 - y) = 0 & \\implies x^2 = y / a \\\\
+                    \\end{aligned}`}
+
+                    The three equations aren't independent (combining the first and third one gives the second one). This is because we didn't 
+                    use the most general form for curves which would give us 3 independent equations we'd need to solve. So now we see that 
+                    the intersection object represents $x^2 = b / a$ and $y = b$. This has two solutions {`$x_{1,2} = \\pm \\sqrt{\\frac{b}{a}}$`} as expected 
+                    and the solutions are the correct ones too (eg. $y$ is just the vertical offset $b$ of the line).
+                </div>
+                <div>
+                    Great, we have shown that what we learnt makes sense and can be applied without much thought required. We could even plot the solutions 
+                    using a library that can plot the null space of the algebraic equations implicitly.
+                </div>
+                <h5>Intersecting Elliptic Curves</h5>
                 <div>
                     To be continued.
                 </div>
             </div>
-
+            <h4>Conclusion</h4>
+            <div>
+                We have learnt how to represent arbitrary polynomials using GA and that intersection and joining them works and makes sense. 
+                There are some other things that we could explore. For example nothing stops us from using more complicated up functions than 
+                polynomials and there is no reason they should not make sense anymore.
+            </div>
+            <div>
+                The major thing we are still missing are operations on objects such as rotation and translation for which we want rotors. 
+                For these the metric and whether we use a dual representation or not will actually matter. That will be the topic of the next section.
+            </div>
             <h4><Link to="/ga-design-2">Next: Design of Geometric Algebras - Part 2</Link></h4>
         </div>
     )
